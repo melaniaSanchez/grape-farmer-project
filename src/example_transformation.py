cc9ca3588f6ff97cca6e81c45ef8054ed775a547
melaniaSanchez <melania.s@thoughtworks.com>
2022-05-17 11:59:05 +0200

    Add dags with mwaa and emr to orchestrate spark job

--- src/example_transformation.py
+++ src/example_transformation.py
@@ -1,4 +1,27 @@
+import argparse
+
+from pyspark.sql import SparkSession
+
+def setup_spark():
+    return SparkSession \
+        .builder \
+        .appName("ExampleTransformations") \
+        .config("spark.sql.caseSensitive", True) \
+        .enableHiveSupport() \
+        .getOrCreate()
+
+
+def transformation(data_frame):
+    return
+
+
 
 def function_a (**kwargs):
+    spark = setup_spark()
+
+    input_df = spark.read.csv()
+
+    transformation(input_df)
+
     name = kwargs['name']
     return f'hello {name} !!'
